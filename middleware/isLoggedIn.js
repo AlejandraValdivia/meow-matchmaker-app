a18078b9d16ee77b68cee2d6ce45beac6b4fe041
Alejandra Valdivia <alexvcodes@gmail.com>
2024-05-22 22:39:26 -0700

    feat: add controllers/auth and controllers/pokemon to separate routes and keep server.js more organized

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,6 +1,6 @@
 function isLoggedIn(req, res, next) {
     if (!req.user) {
-        req.flash('error', 'You must be signed in to view this page');
+        req.flash('error', 'You must be signed in to view this page. Please login...');
         res.redirect('/auth/login');
     } else {
         next();
--- server.js
+++ server.js
@@ -1,96 +1,65 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const flash = require("connect-flash");
-const session = require("express-session");
-const passport = require("./config/passport-config");
-const isLoggedIn = require("./middleware/isLoggedIn");
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('./config/passport-config');
+const isLoggedIn = require('./middleware/isLoggedIn');
 const SECRET_SESSION = process.env.SECRET_SESSION;
 const PORT = process.env.PORT || 3000;
 
 // import model
 const { User } = require('./models');
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
-app.use(
-  session({
+app.use(express.static(__dirname + '/public'));
+app.use(session({
     secret: SECRET_SESSION,
     resave: false,
-    saveUninitialized: true,
-  })
-);
+    saveUninitialized: true
+}));
 app.use(flash());
 
-// initialize passport
+// initial passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 // middleware for tracking users and alerts
 app.use((req, res, next) => {
-  res.locals.alerts = req.flash();
-  res.locals.currentUser = req.user;
-  next(); // next() means go to the said route
+    res.locals.alerts = req.flash();
+    res.locals.currentUser = req.user;
+    next(); // going to said route
 });
 
-app.get("/", (req, res) => {
-  res.render("home", {});
+app.get('/', (req, res) => {
+    res.render('home', {});
 });
 
-// ======= GET ROUTES =======
-app.get("/auth/signup", (req, res) => {
-  res.render("auth/signup", {});
+// import auth routes
+app.use('/auth', require('./controllers/auth'));
+// app.use('/pokemon', require('./controllers/pokemon'));
+// app.use('/', require('./controllers/pokemon'));
+
+// --- AUTHENTICATED ROUTE: go to user profile page --- 
+app.get('/profile', isLoggedIn, (req, res) => {
+    const { name, email, phone } = req.user;
+    res.render('profile', { name, email, phone });
 });
 
-// --- go to login page ---
-app.get("/auth/login", (req, res) => {
-  res.render("auth/login", {});
-});
+// any authenticated route will need to have isLoggedIn before controller
+// app.get('/pokemon', isLoggedIn, (req, res) => {
+//     // get data
+//     // render page + send data to page
+// });
 
-// ======= POST ROUTES =======
-// --- grab data from req.body and create user + redirect + error handling
-// --- we need the name, phone, password and email
-app.post("/auth/signup", async (req, res) => {
-  res.send(req.body);
-  // create phone number error, then we can address a solution
-  // search for the email in database (unique)
-  try {
-    const findUser = await User.findOne({ email: req.body.email });
-    // if findUser is null, then we can create a new user
-    if (!findUser) {
-      const newUser = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: req.body.password,
-      });
+// app.get('/pokemon/:id/edit', isLoggedIn, (req, res) => {});
 
-      // authenticate the user via passport
-      passport.authenticate('local', {
-        successRedirect: '/',
-        successFlash: `Welcome ${newUser.name}! Account created.`
-      })(req, res);
-      
-    } else {
-        req.flash('error', 'Email already exists. Try another email');
-        res.redirect('/auth/signup');
-    }
-  } catch (error) {
-    console.error("--- ERROR IN SINGUP POST----\n", error);
-    if (error.errors.phone.name === "validatorError") {
-        req.flash('error', 'Phone number needs to be in format XXX-XXX-XXXX');
-        res.redirect('/auth/signup');
-    }
-  }
-});
+// app.delete('/pokemon/:id', isLoggedIn, (req, res) => {});
 
-app.post("/auth/login", (req, res) => {
-  res.send(req.body);
-});
 
 const server = app.listen(PORT, () => {
-  console.log("You are listening 🎧 on PORT", PORT);
+    console.log('🏎️ You are listening on PORT', PORT);
 });
 
 module.exports = server;
